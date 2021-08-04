07af5ce1e95a3123c9de8098b49221dd33ddc63b
ryliespriggs <rspriggs812@gmail.com>
2021-08-03 18:12:04 -0700

    adding to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,8 +15,26 @@ class ReviewsController < ApplicationController
     end
 
     def create
+        @review = Review.new(review_params)
+
+        if @review.save
+            redirect_to @review 
+        else
+            render 'new'
+        end
     end
 
-    
+    def update
+        @review.update(review_params)
+    end
 
+    def destroy
+        @review.destroy 
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :comment)
+    end
 end
