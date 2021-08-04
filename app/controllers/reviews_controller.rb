af978332737ad8462b2f1df05d3a649cce1c2564
ryliespriggs <rspriggs812@gmail.com>
2021-08-03 18:09:03 -0700

    adding to shows table

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,22 @@
 class ReviewsController < ApplicationController
 
+    def index 
+        @reviews = Review.all 
+    end
+
+    def new 
+        @review = Review.new
+    end
+
+    def show 
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    
+
 end
--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,35 @@
 class ShowsController < ApplicationController
 
+    def index
+        @shows = Show.all
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def show
+        @show = Show.find(params[:id])
+    end
+
+    def create
+    end
+
+    def edit 
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    private 
+
+    def show_params
+
+    end
+
 end
