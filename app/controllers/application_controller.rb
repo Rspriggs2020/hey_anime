54f32bcf2689c4c98c42820a4a7333600a06f418
ryliespriggs <rspriggs812@gmail.com>
2021-08-07 13:31:15 -0700

    secrets, session, testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    def welcome
+        redirect_to controller: 'sessions', action: 'new' unless session[:name]
+    end
     
     def current_user
         session[:name]
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   resources :shows
   resources :reviews
   resources :watch_later
+  
+  get 'secrets/new'
+
+  root 'application#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 end
