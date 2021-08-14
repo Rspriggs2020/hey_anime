ab4cf273863ece8ce224aa3815d0ac3e92afa52e
ryliespriggs <rspriggs812@gmail.com>
2021-08-14 15:26:01 -0700

    review views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,9 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
 
-
     private 
 
     def redirect_if_not_logged_in
         redirect_to '/login' unless current_user
     end
-    #helper_method :current_user
-    #helper_method :logged_in? 
-
-
-    #def current_user
-    #    @current_user = (User.find_by(id: session[:user_id]) || User.new)
-    #end  
-
-    #def require_user
-    #    redirect_to '/login' unless current_user
-    #end
-
-
-   # def logged_in?
-    #    session[:user_id] 
-    #end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :title, presence: true
+    validates :genre, presence: true 
+    validates :description, presence: true 
+    validates :seasons, presence: true
+    validates :rating, presence: true 
 end
